585f56976c14ad247d4a4023afce3ccd895acc06
Drkgrey <ddrkgrey@gmail.com>
2018-01-19 08:41:47 +0300

    fix2

---
+++ SplashScreenForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asteroid_game
+{
+    public partial class SplashScreenForm : Form
+    {
+        public SplashScreenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void Exit_button_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        private void Record_button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
